bba3a25eecc30a6d16c210201ba4d3fee589f1ac
Spilka Tim <spilkat@ict-campus.net>
2018-05-02 15:20:05 +0200

    Klasse Game erstellt an PowerUp gearbeitet

--- Tank_game/src/net/ictcampus/view/PowerUp.java
+++ Tank_game/src/net/ictcampus/view/PowerUp.java
@@ -3,27 +3,68 @@ package net.ictcampus.view;
 import net.ictcampus.model.Element;
 
 public class PowerUp extends Element{
+	
+	//Definition
+	private int xPos;
+	private int yPos;
 	private int zufallPos;
 	private int zufallPow;
 	private double ugZufallPos;
 	private double ugZufallPow;
 	
-	public PowerUp(){
+	//Element wird erstellt
+	public PowerUp(int posX, int posY, int width, int height, String pic){
 		super(posX, posY, width, height, pic);
 	}
 	
-	public int zufallPosition() {
+	//
+	public void zufallPosition() {
 		ugZufallPos = Math.random()*4;
 		ugZufallPos =(Math.round(ugZufallPos));
 		zufallPos = (int)(ugZufallPos);
-		if(zufallPos == 1) {
-			return
+		switch(zufallPos) {
+		case 1:
+			setxPos(63);
+			setyPos(590);
+			break;
+		case 2:
+			setxPos(632);
+			setyPos(64);
+			break;
+		case 3:
+			setxPos(460);
+			setyPos(370);
+			break;
+		case 4:
+			setxPos(406);
+			setyPos(595);
+			break;
+		default:
+			zufallPosition();
 		}
 	}
+	
 	public int zufallPowerUp() {
-		ugZufallPos = Math.random()*4;
-		ugZufallPos =(Math.round(ugZufallPos));
-		zufallPos = (int)(ugZufallPos);
-		
+		ugZufallPow = Math.random()*4;
+		ugZufallPow =(Math.ceil(ugZufallPow));
+		zufallPow = (int)(ugZufallPow);
+		return zufallPow;
 	}
+	
+	public int getxPos() {
+		return xPos;
+	}
+
+	public void setxPos(int xPos) {
+		this.xPos = xPos;
+	}
+
+	public int getyPos() {
+		return yPos;
+	}
+
+	public void setyPos(int yPos) {
+		this.yPos = yPos;
+	}
+	
 }
